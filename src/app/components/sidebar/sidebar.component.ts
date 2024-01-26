fc9fa075b752dbb7e2ff09245f7fb62f3a2aa678
Kertsu <kurtddbigtas@gmail.com>
2024-01-26 20:05:04 +0800

    fixed responsive sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Sidebar } from 'primeng/sidebar';
 
 @Component({
@@ -6,12 +6,37 @@ import { Sidebar } from 'primeng/sidebar';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   @ViewChild('sidebarRef') sidebarRef!: Sidebar;
 
   closeCallback(e:any): void {
       this.sidebarRef.close(e);
   }
+  
+  ngOnInit(): void {
+    this.checkScreenWidth();
+  }
 
-  sidebarVisible: boolean = false;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkScreenWidth();
+  }
+
+  checkScreenWidth(): void {
+    const screenWidth = window.innerWidth;
+
+    // Adjust the threshold based on your requirements
+    if (screenWidth >= 912) {
+      this.modal = false;
+      this.sidebarVisible = true;
+    } else {
+      // You can set different values if the screen width is less than 640px
+      this.modal = true;
+      this.sidebarVisible = false;
+    }
+  }
+  
+
+  sidebarVisible: boolean = true;
+  modal: boolean = false;
 }
