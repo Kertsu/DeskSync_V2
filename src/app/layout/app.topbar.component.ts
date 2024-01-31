a472fa54f613ac9a3b80b0541a6953c1f1676732
Kertsu <kurtddbigtas@gmail.com>
2024-01-31 14:28:52 +0800

    added get notifications

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,15 +1,26 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "./service/app.layout.service";
 import { UiService } from '../services/ui.service';
 import { UserService } from '../services/user.service';
 import { SocketService } from '../services/socket.service';
+import { WebService } from '../services/web.service';
+
+interface Notification {
+    id: string,
+    user: string,
+    message: string,
+    status: string,
+}
 
 @Component({
     selector: 'app-topbar',
     templateUrl: './app.topbar.component.html'
 })
-export class AppTopBarComponent {
+export class AppTopBarComponent implements OnInit{
+
+    notifications: Notification[] = []
+    totalDocuments!: number;
 
     isDark:boolean = false;
 
@@ -21,7 +32,11 @@ export class AppTopBarComponent {
 
     @ViewChild('topbarmenu') menu!: ElementRef;
 
-    constructor(public layoutService: LayoutService, private uiService:UiService, private userService: UserService, private socketService: SocketService) { }
+    constructor(public layoutService: LayoutService, private uiService:UiService, private userService: UserService, private socketService: SocketService, private webService: WebService) { }
+
+    ngOnInit(): void {
+        this.getSelfNotifications({first: 0})
+    }
 
     changeTheme(theme: string){
         this.uiService.switchTheme(theme);
@@ -31,5 +46,15 @@ export class AppTopBarComponent {
     logout(){
         this.socketService.emit('die',{})
         this.userService.logout();
+    }
+
+    getSelfNotifications(event: any){
+        this.webService.getNotifications(event.first || 0).subscribe({
+            next: (res:any)=>{
+                this.notifications = res.notifications
+                this.totalDocuments = res.totalDocuments
+            }, error: (error) => {
+            }
+        })
     }
 }
--- src/app/services/web.service.ts
+++ src/app/services/web.service.ts
@@ -2,18 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WebService {
+  baseUserURL: string = 'http://localhost:8000/api/users';
+  constructor(private http: HttpClient) {}
 
-  baseUserURL: string = "http://localhost:8000/api/users"
-  constructor(private http: HttpClient) { }
-
-  onLoginUser(userData: any ){
-    return this.http.post(`${this.baseUserURL}/login`, userData)
+  onLoginUser(userData: any) {
+    return this.http.post(`${this.baseUserURL}/login`, userData);
   }
 
-  getSelf(){
-    return this.http.get(`${this.baseUserURL}/self`)
+  getSelf() {
+    return this.http.get(`${this.baseUserURL}/self`);
+  }
+
+  getNotifications(skip: number) {
+    const limit = skip;
+
+    const page = Math.floor(skip / limit) + 1;
+    return this.http.get(`${this.baseUserURL}/self/notifications?page=${page}&limit=${limit}`);
   }
 }
